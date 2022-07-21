87da9bfd15508c8e98e6b35c1a4884698a840b3f
tusharhirapure <tush48545@gmail.com>
2022-07-21 19:26:03 +0530

    home added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    UserDashboardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
